f7be1143d7d949868e3332660af647146ad129be
molpe <alberto.molpeceres@gmail.com>
2015-04-17 15:46:35 +0200

    new fields on subscriptions

--- lib/besepa/subscription.rb
+++ lib/besepa/subscription.rb
@@ -6,7 +6,8 @@ module Besepa
     include Besepa::ApiCalls::Create
     include Besepa::ApiCalls::Destroy
         
-    FIELDS = [:id, :reference, :status, :metadata]
+    FIELDS = [:id, :last_debit, :next_debit, :status, :metadata, :starts_at, :renew_at]  
+    
     
     FIELDS.each do |f|
       attr_accessor f
