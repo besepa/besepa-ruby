4a4f8cdc79d5a5e6a11ca30c9024ff0c612b6ad9
molpe <alberto.molpeceres@gmail.com>
2015-04-17 15:47:05 +0200

    new Remittance model

---
+++ lib/besepa/remittance.rb
@@ -0,0 +1,32 @@
+module Besepa
+
+  class Remittance < Besepa::Resource
+    
+    include Besepa::ApiCalls::List
+        
+    FIELDS = [:id, :collect_at, :send_at, :sent_at, :status, :scheme]
+    
+    attr_accessor :bank_account
+    
+    FIELDS.each do |f|
+      attr_accessor f
+    end    
+      
+    def self.api_path(filters={})
+      "/remittances"
+    end
+    
+    protected
+    
+      def process_attributes(attrs)
+        self.class::FIELDS.each do |key|
+          self.send("#{key.to_s}=", attrs[key.to_s] || attrs[key.to_sym])
+        end
+        self.bank_account = Besepa::BusinessAccount.new(attrs['bank_account']) if attrs['bank_account']
+        process_activities(attrs)
+        self
+      end
+    
+
+  end
+end
