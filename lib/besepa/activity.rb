cbcd6fb5d1393e5ffdfad5cc2e7d268ed2db9357
molpe <alberto.molpeceres@gmail.com>
2015-04-29 16:10:09 +0200

    get trackable in activities

--- lib/besepa/activity.rb
+++ lib/besepa/activity.rb
@@ -2,7 +2,7 @@ module Besepa
 
   class Activity < Besepa::Resource
         
-    FIELDS = [:id, :key, :created_at, :owner]
+    FIELDS = [:id, :key, :created_at, :owner, :trackable]
     FIELDS.each do |f|
       attr_accessor f
     end    
