01dfd78cd10cae18ec43bfc4fe84782e0735f088
Jose Galisteo <ceritium@gmail.com>
2016-05-04 17:08:17 +0200

    activities

--- lib/besepa/activity.rb
+++ lib/besepa/activity.rb
@@ -1,11 +1,17 @@
 module Besepa
-
   class Activity < Besepa::Resource
-        
+
     FIELDS = [:id, :key, :created_at, :owner, :trackable]
     FIELDS.each do |f|
       attr_accessor f
-    end    
-      
+    end
+
+    include Besepa::ApiCalls::List
+
+    class << self
+      def api_path(filters={})
+        '/activities'
+      end
+    end
   end
 end
