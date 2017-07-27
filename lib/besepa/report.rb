188ef98f421073157ca34135906c0e200dae9a72
Rocío Fernandez <rocio@besepa.com>
2017-07-27 11:35:30 +0200

    add download call

--- lib/besepa/report.rb
+++ lib/besepa/report.rb
@@ -6,10 +6,21 @@ module Besepa
     include Besepa::ApiCalls::Create
     include Besepa::ApiCalls::Search
 
-    FIELDS = [:id, :until_at, :since_at, :kind, :created_at]
+    FIELDS = [:id, :until_at, :since_at, :kind, :created_at,
+              :document_file_name, :document_file_size, :status]
 
     FIELDS.each do |f|
       attr_accessor f
+    end
+
+    def self.klass_name
+      "report"
+    end
+
+
+    def download_url
+      response = get "/#{self.class.api_path}/#{CGI.escape(id)}/download"
+      response["response"]["url"]
     end
 
   end
